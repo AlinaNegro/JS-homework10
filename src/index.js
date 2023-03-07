aadb3de7141f0c2013c0909339d86a2c710bac5e
Alina Negro <alinanegro@mail.ru>
2023-03-07 19:36:44 +0300

    add task 2

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ function getMessage() {
 getMessage();
 
 //Задание 2. Создайте галерею переключения изображений. В ней будет две кнопки: вперёд и назад, которые должны переключать изображение.
+
+function Previous() {
+  document.getElementById('garryPotter')
+  .src="img/1.jpg";
+  }
+
+function Next() {
+document.getElementById('garryPotter')
+.src="img/2.jpg";
+}
